a5bbe916267c740484a842d6d334cae7c112020a
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-02-25 01:08:24 -0300

    Update README.MD

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# apirest
+## APIREST
 This project created for studing API REST with SLIM Framework v2 and works.<br>
 I using Insomnia for tests on api requisition.<br>
 I using composer too.<br>
+
+## INSTALL
+INSTALL [APIREST PHP WITH SLIM V2](https://github.com/gleysonabreu/apirest)<br>
+INSTALL WAMPSERVER, XAMP OR ANOTHER APACHE SERVER WITH PHP7.
+Clone or download zip this directory and move to folder www or public_html folder in your computer.
+Start your apache server.
+Import db_apirest.sql in your mariadb.
 Use this command for download all dependencies:
 ```
 $ composer update
---
+++ db_apirest.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 25-Fev-2020 às 04:06
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_apirest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_token` int(11) NOT NULL,
+  `permission` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_token` (`id_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `id_token`, `permission`) VALUES
+(1, 2, 3),
+(2, 2, 4),
+(3, 2, 1),
+(4, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+CREATE TABLE IF NOT EXISTS `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(200) NOT NULL,
+  `valid` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `token`
+--
+
+INSERT INTO `token` (`id`, `token`, `valid`) VALUES
+(2, 'Gla123', '2529360000');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `company` varchar(70) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `lastname`, `company`) VALUES
+(79, 'Lewis', 'Hamilton', 'McLaren'),
+(80, 'Sebastian', 'Vettel', 'Ferrari');
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `permissions`
+--
+ALTER TABLE `permissions`
+  ADD CONSTRAINT `fk_id_token` FOREIGN KEY (`id_token`) REFERENCES `token` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
