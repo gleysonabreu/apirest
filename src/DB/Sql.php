f99450f6673147916e254473d07035195049de60
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-02-26 13:02:44 -0300

    Add postgres support

--- src/DB/Sql.php
+++ src/DB/Sql.php
@@ -1,21 +1,31 @@
 <?php
+
 namespace GLEYSON\DB;
 
 class Sql{
+
+  /* MY SQL CONECTION
   const HOST = "127.0.0.1:3307";
   const USERNAME = 'root';
   const PASSWORD = '';
   const DB = 'db_apirest';
+  */
 
   private $conn;
 
   public function __construct(){
 
+    /*
+    MYSQL CONNECTION
     $this->conn = new \PDO(
       "mysql:dbname=".Sql::DB.";host=".Sql::HOST,
       Sql::USERNAME,
       Sql::PASSWORD
-    );
+    );*/
+
+    $db = (parse_url(getenv('DATABASE_URL') ?: 'postgresql://postgres:32841516@localhost:5432/api_rest'));
+
+    $this->conn = new \PDO("pgsql:" . "host=".$db['host'].";port=".$db['port'].";user=".$db['user'].";password=".$db['pass'].";dbname=".ltrim($db["path"], "/"));
 
   }
 
